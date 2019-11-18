e1cd529e76f6a15409d87306bd57dedfa89f732d
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-18 12:50:18 +0000

    Done.

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ class Tree{
 }
 
 
-class Deciduous{
-  constructor(name){
-    super(definition())
+class Deciduous extends Tree{
+  constructor(species,name){
+    super(species)
     this.name = name
   }
   static definition(){
-    return super.definition();
+   return super.definition() + " Deciduous trees shed their leaves annually.";
+  }
+}
+
+
+class Evergreen extends Tree{
+    constructor(species,name){
+    super(species)
+    this.name = name
+  }
+    static definition(){
+   return super.definition() + " Evergreens keep their leaves all year round.";
   }
 }
