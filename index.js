696211ae313bc335d23980ae57fb671806558808
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-18 12:44:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your classes here
+class Tree{
+  constructor(species){
+    this.species = species;
+  }
+  
+  static definition(){
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves."
+  }
+}
+
+
+class Deciduous{
+  constructor(name){
+    super(definition())
+    this.name = name
+  }
+  static definition(){
+    return super.definition();
+  }
+}
